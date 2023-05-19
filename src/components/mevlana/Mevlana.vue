f4ab0e5930d6e620c086de7a7082e65c3d756bc0
Mustafa Daglioglu <mustafa.psd@yandex.com>
2023-05-19 16:45:46 +0300

    Add Mevlana page

---
+++ src/components/mevlana/Mevlana.vue
@@ -0,0 +1,104 @@
+<template>
+    <div class="container pt-4 mt-4">
+        <div class="row">
+            <div class="col">
+                <h1>Mevlana</h1>
+
+                <hr>
+            </div>
+
+            <div class="col-12 mb-4">
+                <article>
+                    <section>
+                        <h2>Who is Mevlana Rumi?</h2>
+                        <p>
+                            Mevlana Rumi, also known as Jalal ad-Din Muhammad Rumi, was a 13th-century Persian poet, Islamic
+                            jurist, and theologian. He is widely recognized as one of the greatest spiritual masters and
+                            poets
+                            of all time.
+                        </p>
+                    </section>
+                </article>
+
+
+                <section>
+                    <h2>Teachings and Philosophy</h2>
+                    <p>
+                        Mevlana Rumi's teachings revolve around the themes of love, unity, and spiritual enlightenment.
+                        He emphasized the importance of connecting with the divine through love and finding unity with
+                        all beings, transcending boundaries of religion, culture, and nationality.
+                    </p>
+                </section>
+            </div>
+        </div>
+    </div>
+
+    <div class="bg-dark">
+        <div class="container py-4">
+            <div class="row">
+                <div class="col-12 col-lg-8 d-flex align-items-center">
+                    <h1 class="text-light">
+                        "Let yourself be silently drawn by the strange pull of what you really love. It will not lead you
+                        astray."
+                        <br> - Mevlana
+                    </h1>
+                </div>
+                <div class="col-12 col-lg-4">
+                    <img src="https://www.bizevdeyokuz.com/wp-content/uploads/mevlana-celaleddin-rumi.jpg" alt="mevlana"
+                        class="h-100 img-fluid">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col-12 col-lg-8 order-first order-lg-last">
+                <article>
+                    <section>
+                        <h2>Sufism and Mevlevi Order</h2>
+                        <p>
+                            Mevlana Rumi's teachings became the foundation of the Mevlevi Order, also known as the Whirling
+                            Dervishes. This Sufi order practices a form of mystical dance called Sama, symbolizing the
+                            spiritual journey of seeking union with the divine. The Mevlevi Order continues to exist today,
+                            carrying forward the legacy of Mevlana Rumi.
+                        </p>
+                    </section>
+
+                    <section>
+                        <h2>Poetry and Literary Works</h2>
+                        <p>
+                            Mevlana Rumi's poetry is considered profound and transformative. His major work, the "Mathnawi,"
+                            is a collection of spiritual poetry and stories that explore deep spiritual and philosophical
+                            concepts in a poetic and accessible manner. His poetry has been widely translated and continues
+                            to inspire people around the world.
+                        </p>
+                    </section>
+
+                    <section>
+                        <h2>Legacy and Impact</h2>
+                        <p>
+                            Mevlana Rumi's teachings and poetry have had a profound impact on spirituality, literature, and
+                            cultural understanding. His messages of love, tolerance, and unity continue to resonate with
+                            people from diverse backgrounds, transcending time and geographical boundaries.
+                        </p>
+                    </section>
+                </article>
+            </div>
+
+
+            <div class="col-12 col-lg-4 order">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/a/a0/Mevlana_Statue%2C_Buca.jpg" alt="mevlana"
+                    class="img-fluid">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "mevlana-component",
+};
+
+</script>
--- src/main.js
+++ src/main.js
@@ -2,15 +2,17 @@ import "bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
 import { createApp } from 'vue'
 import * as VueRouter from 'vue-router'
+import AppVue from "./App.vue"
 import About from './components/about/About.vue'
 import Resume from './components/resume/Resume.vue'
 import Homeland from './components/homeland/Homeland.vue'
-import AppVue from "./App.vue"
+import Mevlana from './components/mevlana/Mevlana.vue'
 
 const routes = [
     { path: '/', component: About },
     { path: '/resume', component: Resume },
     { path: '/homeland', component: Homeland },
+    { path: '/mevlana', component: Mevlana },
 ]
 
 const router = VueRouter.createRouter({
